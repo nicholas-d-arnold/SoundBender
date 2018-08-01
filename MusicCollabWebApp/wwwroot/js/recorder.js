1aabf097acafa54d2da5fab6d5e129b99a3cb3e9
amiller <adam5bucks475@gmail.com>
2018-07-31 22:31:27 -0400

    refixing the recorder

--- MusicCollabWebApp/wwwroot/js/recorder.js
+++ MusicCollabWebApp/wwwroot/js/recorder.js
@@ -52,42 +52,17 @@ function onRecordingReady(e) {
     audio.src = URL.createObjectURL(e.data);
     audio.play();
     
-    
+    saveit();
 }
 
 function saveit() {
     var fileData = new FormData();
     fetch(audio.src).then(response => response.blob())
         .then(blob => {
-            fileData.append("file", blob, "file.mp3");
+            fileData.append("file", blob, "file.ogg");
             fileData.append('name', "test");
             fileData.append("description", "something here");
-            return fetch("/Home/RecordFile", { method: "POST", body: fd })
+            return fetch("/Home/RecordFile", { method: "POST", body: fileData })
         })
-
-
-    // let's do a vanilla JS ajax xhr call, why not?
-    //var xhr = new XMLHttpRequest();
-    //xhr.open('POST', '/Home/RecordFile', true);
-    //xhr.send(fileData);
-    //xhr.onreadystatechange = function () {
-    //    if (xhr.readyState == 4 && xhr.status == 200) {
-    //        alert(xhr.responseText);
-    //        var respJson = JSON.parse(resp);
-    //    }
-    //}
-    setTimeout(function () {
-        $.ajax({
-            method: 'POST',
-            data: fileData,
-            url: "/Home/RecordFile",
-            processData: false,
-            contentType: false,
-            success: function (data) {
-                
-                window.location.href = data;
-            }
-
-        })
-    }, 2000);
+    
 }
