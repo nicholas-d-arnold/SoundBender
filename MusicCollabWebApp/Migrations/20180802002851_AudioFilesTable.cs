c693c3fd5a3b3c2996bd39c8f670be1b39219cc2
Unknown <adam5bucks475@gmail.com>
2018-08-01 20:54:49 -0400

    Migrations for AudioFile table, and putting Identity tables into separate DB

---
+++ MusicCollabWebApp/Migrations/20180802002851_AudioFilesTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MusicCollabWebApp.Migrations
+{
+    public partial class AudioFilesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AudioFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    User = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AudioFiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AudioFiles");
+        }
+    }
+}
---
+++ MusicCollabWebApp/Models/AudioFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCollabWebApp.Models
+{
+    public class AudioFile
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
